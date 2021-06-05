ace4c62890b10458b0261ed6495ef6da21ddae44
keniiy <kehindekehinde894@gmail.com>
2021-06-05 20:18:01 +0100

    delete fininshed tut🤓

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ProductsService } from './product.service';
 
 @Controller('products')
@@ -37,6 +45,12 @@ export class ProductsController {
     @Body('price') prodPrice: number,
   ) {
     this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return null;
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodId: string) {
+    this.productsService.deleteProduct(prodId);
     return null;
   }
 }
